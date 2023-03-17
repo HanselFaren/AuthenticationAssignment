49b5a7050467a2c7dd8247be4c461e5be3c461c7
HanselFaren <91465998+HanselFaren@users.noreply.github.com>
2023-03-17 20:21:00 +0700

    Add files via upload

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { auth, signInWithEmailAndPassword, signInWithGoogle } from "../firebase"
+import { auth, logInWithEmailAndPassword, signInWithGoogle } from "../firebase"
 import { useAuthState } from "react-firebase-hooks/auth";
 import "../styles/Login.css";
 
@@ -14,7 +14,7 @@ function Login() {
       // maybe trigger a loading screen
       return;
     }
-    if (user) navigate.replace("/dashboard");
+    if (user) navigate("/dashboard");
   }, [user, loading]);
   return (
     <div className="login">
@@ -35,7 +35,7 @@ function Login() {
         />
         <button
           className="login__btn"
-          onClick={() => signInWithEmailAndPassword(email, password)}
+          onClick={() => logInWithEmailAndPassword(email, password)}
         >
           Login
         </button>
